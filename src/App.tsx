a88dadd8725ba1ef19cdc4827edb8f9270a686c9
abhinav <ambhinav@yahoo.com>
2021-02-25 03:36:52 +0800

    attempt smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Divider, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+//import { Link } from 'react-scroll'
 import './App.css';
 import About from './components/About';
 import Experience from './components/Experience';
@@ -12,18 +13,34 @@ function App() {
       <Grid
         container direction="column" alignItems="center" spacing={10}
        >
+        <Grid item xs={12}>
+				  <Typography variant="h2" className="sectionHeader">Abhinav Ramnath</Typography>
+			  </Grid>
+        {/* <nav>
+          <Grid container justify="center" spacing={4}>
+              <Grid item>
+                <Link activeClass="active" to="experience" spy={true} smooth={true} duration={500}>Experience</Link>
+              </Grid>
+              <Grid item>
+                <Link activeClass="active" to="projects" spy={true} smooth={true} duration={500}>Projects</Link>
+              </Grid>
+              <Grid item>
+                <Link activeClass="active" to="skills" spy={true} smooth={true} duration={500}>Skills</Link>
+              </Grid>
+          </Grid>
+        </nav> */}
         <About />
-        <Grid item style={{ maxWidth: "1600px" }}>
+        <Grid item style={{ maxWidth: "1600px" }} id="experience">
           <Typography variant="h3" className="sectionHeader">Experience</Typography>
           <Divider style={{ margin: "20px" }}></Divider>
           <Experience />
         </Grid>
-        <Grid item style={{ maxWidth: "1200px" }}>
+        <Grid item style={{ maxWidth: "1200px" }} id="projects">
           <Typography variant="h3" className="sectionHeader">Projects</Typography>
           <Divider style={{ margin: "20px" }}></Divider>
           <Projects />
         </Grid>
-         <Grid item style={{ maxWidth: "1200px" }}>
+        <Grid item style={{ maxWidth: "1200px" }} id="skills">
           <Typography variant="h3" className="sectionHeader">Skills</Typography>
           <Divider style={{ margin: "20px" }}></Divider>
           <Skills />
